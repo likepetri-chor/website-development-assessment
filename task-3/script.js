67fd6f43ef0fe26edbfeb06eacb6438814337643
likepetri_chor <167917205+likepetri-chor@users.noreply.github.com>
2025-12-17 13:16:28 +0000

    Updated script.js

--- task-3/script.js
+++ task-3/script.js
@@ -1,7 +1,11 @@
 let dark_mode_enabled
 const display_mode_button = document.querySelector(".display-mode-button");
 
+const logo = document.querySelector(".logo");
+
+/*Wait and listen for an event from the display mode button.*/
 dark_mode_toggle.addEventListener("click", () => {
+	/*Invert the property value, which turns it on or off.*/
 	dark_mode_enabled = !dark_mode_enabled
 
     if (dark_mode_enabled) {
