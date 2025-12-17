490057e03cb6c86b341937628862cf4dc645607a
likepetri_chor <167917205+likepetri-chor@users.noreply.github.com>
2025-12-17 13:11:31 +0000

    Created script.js

---
+++ task-3/script.js
@@ -0,0 +1,12 @@
+let dark_mode_enabled
+const display_mode_button = document.querySelector(".display-mode-button");
+
+dark_mode_toggle.addEventListener("click", () => {
+	dark_mode_enabled = !dark_mode_enabled
+
+    if (dark_mode_enabled) {
+
+    } else {
+
+    }
+})
